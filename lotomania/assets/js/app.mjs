9968ffe4cc7a184d8495ebb558c3349eefb64e8c
joelgaldino <joel.galdino@gmail.com>
2026-09-11 10:41:06 -0300

    Layout e botoes

--- lotomania/assets/js/app.mjs
+++ lotomania/assets/js/app.mjs
@@ -1,6 +1,18 @@
 export class app {
 
     constructor(){
-        console.log('teste')
+        this.tela()
+    }
+
+    tela(){
+        const app = document.querySelector('main')
+
+        let bloco = ''
+        for (let i = 1; i <= 100; i++){
+            let numero = i < 10 ? `0${i}` : i == 100 ? `00` : i
+            bloco += `<button value="${numero}">${numero}</button>`
+        }
+
+        app.innerHTML = bloco
     }
 }
